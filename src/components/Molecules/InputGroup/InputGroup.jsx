756a1fad64fa3a0a6b419329771d60eec2b3b31c
Julian Barbosa <bjvlixn@gmail.com>
2022-02-07 23:40:05 -0300

    fix InputGroup export mode

--- src/components/Molecules/InputGroup/InputGroup.jsx
+++ src/components/Molecules/InputGroup/InputGroup.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "../../Atoms/Input";
 
-const InputGroup = () => {
-  return <div>InputGroup</div>;
+export const InputGroup = () => {
+  const [value, setValue] = useState("");
+
+  return (
+    <div>
+      <Input value={value} onChange={setValue} placeholder="Pokemon" />
+    </div>
+  );
 };
-
-export default InputGroup;
