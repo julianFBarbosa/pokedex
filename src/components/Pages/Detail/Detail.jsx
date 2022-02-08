e7bf75b9761ec5c189c8331e49c680c881931536
Julian Barbosa <bjvlixn@gmail.com>
2022-02-07 23:40:55 -0300

    wrap Home template

--- src/components/Pages/Detail/Detail.jsx
+++ src/components/Pages/Detail/Detail.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { Home } from "../../Templates/Home";
 
 export const Detail = () => {
   const { id } = useParams();
 
   return (
-    <div>
-      <p>Detail</p>
-      <p>Pokémon: {id}</p>
-      <Link to="/">Home</Link>
-    </div>
+    <Home>
+      <div>
+        <p>Detail</p>
+        <p>Pokémon: {id}</p>
+        <Link to="/">Home</Link>
+      </div>
+    </Home>
   );
 };
