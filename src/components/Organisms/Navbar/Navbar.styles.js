e2e3b2140ef4a66cc985a56cb71bdbfb48a7be32
Julian Barbosa <bjvlixn@gmail.com>
2022-03-01 00:00:13 -0300

    update navbar styles

--- src/components/Organisms/Navbar/Navbar.styles.js
+++ src/components/Organisms/Navbar/Navbar.styles.js
@@ -1,14 +1,22 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Wrapper = styled.nav`
   background: #f5f5f5;
+  box-shadow: 0px 1px 5px 0px #dfdfdf;
+  margin-bottom: 16px;
 `;
 
 export const Container = styled.nav`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 1080px;
   max-width: 95%;
   margin: 0 auto;
-  padding: 12px 0;
+  padding: 24px 0;
+
+  ${media.lessThan("medium")`
+    flex-direction: column;
+  `}
 `;
