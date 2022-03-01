dca1d0853b98604768ccd79b9b3579a826ce717b
Julian Barbosa <bjvlixn@gmail.com>
2022-02-28 23:57:36 -0300

    add api calls to list pokemons and get data from a single one

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,18 @@
-export const BASE_URL = "https://pokeapi.co/api/v2/";
+export const BASE_URL = "https://pokeapi.co/api/v2";
 
+export const getPokemonList = async (limit = 40, offset = 0) => {
+  const request = await fetch(
+    `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`
+  );
+  const response = await request.json();
+
+  return response;
+};
+
+export const getPokemonData = async (name = false) => {
+  if (!name) return;
+  const request = await fetch(`${BASE_URL}/pokemon/${name}`);
+  const response = await request.json();
+
+  return response;
+};
