6a30d5a89706d2ef33c5f528685fd94411a04e79
Julian Barbosa <bjvlixn@gmail.com>
2022-02-28 23:59:08 -0300

    reorganize import orders, add ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 import "@fontsource/lato/index.css";
 import "./styles/index.scss";
 
 import { Routes } from "./Routes";
+import { colors } from "./GlobalTheme";
 
 const root = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider theme={colors}>
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
   root
 );
